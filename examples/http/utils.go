7f356dab857de6f902c8c99db7062016276bdad1
Balakrishnan <light.bala@gmail.com>
2020-06-28 21:06:15 +0530

    (example) fix http coutAll to handle EOF

--- examples/http/utils.go
+++ examples/http/utils.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"io"
 	"os"
 )
@@ -8,18 +9,28 @@ import (
 // coutAll reads from r and writes the value to stdout.
 // Note: ioutil.ReadAll reads from r and returns the value.
 func coutAll(r io.Reader) error {
-	bufSize := 512
-	buf := make([]byte, bufSize)
+	buf := make([]byte, bytes.MinRead) // bytes.MinRead is 512
 	for {
 		n, err := r.Read(buf)
-		if err != nil {
-			return err
-		}
-		if n != bufSize {
+		if err == io.EOF {
 			os.Stdout.Write(buf[:n])
 			break
 		}
+		if err != nil {
+			return err
+		}
 		os.Stdout.Write(buf)
 	}
+	/*
+		Alternatively we can use `bytes.Buffer`
+		it automatically grows the buffer by `bytes.MinRead` until `io.EOF`
+
+		var buf bytes.Buffer
+		_, err := buf.ReadFrom(r)
+		if err != nil {
+			return err
+		}
+		os.Stdout.Write(buf.Bytes())
+	*/
 	return nil
 }

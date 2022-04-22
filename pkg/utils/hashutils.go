5a65748e425054110b367dc101185ed1b95b36d4
Balakrishnan.S <light.bala@gmail.com>
2022-04-22 14:19:35 +0530

    (utils) add sha256 of file

---
+++ pkg/utils/hashutils.go
@@ -0,0 +1,23 @@
+package utils
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"io"
+	"os"
+)
+
+func Sha256OfFile(filePath string) (string, error) {
+	f, err := os.Open(filePath)
+	if err != nil {
+		return "", err
+	}
+	defer f.Close()
+
+	h := sha256.New()
+	if _, err := io.Copy(h, f); err != nil {
+		return "", err
+	}
+
+	return hex.EncodeToString(h.Sum(nil)), nil
+}
